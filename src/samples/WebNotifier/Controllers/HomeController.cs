ee809fbb5653a210f3df2e6cff0bc91f7dabe6d0
Alexis Castro <alcaab@.com>
2021-06-28 08:39:49 -0400

    refactoring projects

---
+++ src/samples/WebNotifier/Controllers/HomeController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Desyco.Notification;
+using Microsoft.AspNetCore.Mvc;
+using WebNotifier.Service;
+
+namespace WebNotifier.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        private readonly INotificationHost _notificationHost;
+        private readonly IRequestFormService _requestFormService;
+
+        public HomeController(
+            INotificationHost notificationHost,
+            IRequestFormService requestFormService
+        )
+        {
+            _notificationHost = notificationHost;
+            _requestFormService = requestFormService;
+        }
+
+
+        [HttpPost("[action]")]
+        public async Task<IActionResult> AddRequest(RequestFormModel model)
+        {
+
+            //var result = await new RequestFormValidator().ValidateAsync(model);
+            //if (!result.IsValid)
+            //    throw new Exception();
+
+            _requestFormService.Add(model);
+
+            return Ok(await SendNotification(model));
+        }
+
+        [HttpPost("[action]")]
+        public async Task<IActionResult> AssingRequest(string id)
+        {
+             var model = _requestFormService.AssignAgent(id);
+
+             return Ok(await SendNotification(model));
+        }
+
+        [HttpPost("[action]")]
+        public IActionResult Get()
+        {
+            return Ok(_requestFormService.GetAll());
+        }
+
+
+        private async Task<string> SendNotification(RequestFormModel model)
+        {
+
+            var subjectDescription = "Should be dynamic";
+
+            var message = new NotificationMessage
+            {
+                NotificationMethod = NotificationMethod.External,
+                Subject = $"Service Request - {subjectDescription}",
+                To = new List<NotificationAddress>() { new NotificationAddress(model.Email, $"{model.FirstName} {model.LastName}") },
+
+                //Body should be null so you can use template
+                //Body = model.Body,
+
+                //Specify the template key
+                TemplateKey = Utils.GetTemplate(model.Status),
+
+                //You can send object you want available in template design
+                Data = new Dictionary<string, object>
+                {
+                    { "form", model}
+                }
+            };
+
+            //You don't have to wait for notification result
+            await _notificationHost.Notify(message);
+
+
+            return message.Group;
+        }
+
+    }
+}
---
+++ src/templating/Notification.T5Templating/ServiceCollectionExtensions.cs
@@ -0,0 +1,53 @@
+﻿using Desyco.Notification;
+using Desyco.Notification.T5Templating;
+using Desyco.Notification.Templating;
+using Microsoft.Extensions.Logging;
+
+// ReSharper disable once CheckNamespace
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class T5TemplatingCollectionExtensions
+    {
+        public static TemplateOptions UseT5FileTemplating(this NotificationOptions options,string directoryTemplate)
+        {
+
+            options.ConfigureProviderOptions(NotificationConst.TemplateProviderType, directoryTemplate);
+            
+            options.UseTemplateCompiler(
+                sp => new T5CompilerEngine(),
+                sp => new T5CompilerService(sp.GetService<ITemplateEngine>()));
+            
+            options.UseTemplateContent(sp => new FileTemplateContentService(
+                sp.GetService<ITemplateCompilerService>(), sp.GetService<ILoggerFactory>(),
+                sp.GetService<NotificationOptions>()));
+
+            return new TemplateOptions(options);
+        }
+
+        public static TemplateOptions UseT5Templating(this NotificationOptions options)
+        {
+            options.UseTemplateCompiler(
+                sp => new T5CompilerEngine(),
+                sp => new T5CompilerService(sp.GetService<ITemplateEngine>()));
+
+            return new TemplateOptions(options);
+        }
+
+    }
+
+    public class TemplateOptions
+    {
+        public TemplateOptions(NotificationOptions options)
+        {
+            Options = options;
+        }
+
+        public NotificationOptions Options { get; }
+
+        public string TemplateDirectory { get; set; }
+
+
+
+
+    }
+}
